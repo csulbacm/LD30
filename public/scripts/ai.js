50535565c4d8e0b95e12fc582f47f3d902af162f
David Martel <davidkmartel@gmail.com>
2014-08-24 20:07:34 -0700

    started outlining pathfinding code

--- public/scripts/ai.js
+++ public/scripts/ai.js
@@ -79,6 +79,40 @@ Behavior_Follow.prototype.step = function(dT, thisAi) {
 	thisAi.self.p.vy = dy;
 }
 
+function Behavior_Follow_Path(priority, points) {
+	Behavior.call('follow', priority);
+	this.points = points;
+	this.cur = 0;
+	this.thresh = 1;
+}
+
+Behavior_Follow_Path.step = function(dT, thisAi) {
+	var selfx = thisAi.self.p.x;
+	var selfy = thisAi.self.p.y;
+	var otherx = this.points[this.cur].x;
+	var othery = this.points[this.cur].y;
+	var dx = otherx-selfx;
+	var dy = othery-selfy;
+	var distance = (dx*dx) + (dy*dy);
+	var range2 = this.thresh * this.thresh;
+	if(range2 < distance) {
+		var speed = thisAi.self.p.speed;
+		distance = Math.sqrt(distance);
+		if(distance > speed) {
+			dx *= speed/distance;
+			dy *= speed/distance;
+		}
+	} else {
+		dx = 0;
+		dy = 0;
+		this.cur++;
+		if(this.cur == this.points.length)
+			this.cur == 0;
+	}
+	thisAi.self.p.vx = dx;
+	thisAi.self.p.vy = dy;
+}
+
 //shoots a laser at the next step
 //this only an action to create a laser
 function Behavior_Shoot(priority, targetx, targety, range, speed) {
@@ -116,4 +150,44 @@ Behavior_Attack.prototype.step = function(dT, thisAi) {
 		this.time_elapsed -= 1;
 		thisAi.add(new Behavior_Shoot(1, this.target.p.x, this.target.p.y, 100, 200));
 	}
+}
+
+/*
+	Graph format
+	Each node is idenfified by an index. The nodes should be put in an array.
+	Each node has a list of edges. Each edge has an index it connects to and
+	the cost to that node. The cost is 1 for straight connections and 1.4 for diagonals.
+	The cost can be increased if that edge should be advoided but there shouldn't be
+	any need in this program.
+*/
+function node(index, edges) {
+	this.index = index;
+	this.edges = edges;
+}
+
+function edge(index, cost) {
+	this.index = index;
+	this.cost = cost;
+}
+
+function Astar_Node(index, cost, distance) {
+	//index is the index of node this is refering to
+	//cost is the total cost up to this point
+	//distance is the manhatan distance from here to the end
+	this.index = index;
+	this.cost = cost;
+	this.distance = distance;
+}
+
+Astar_Node.prototype.total_cost = funtion() {
+	return cost + distance;
+}
+
+
+function Astar(nodeList) {
+	this.nodeList = nodeList;
+	this.start = 0;
+	this.end = 0;
+	this.open_list = new Array();
+	this.closed_list
 }
